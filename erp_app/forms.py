4521cf210e5fb36e6e72fae9c410a6ca6c8d9b5e
Elena-R-P <ponomarevaer@gmail.com>
2025-08-05 19:14:24 -0500

    Added widgets to forms.py

--- erp_app/forms.py
+++ erp_app/forms.py
@@ -8,24 +8,42 @@ class DriverForm(ModelForm):
     class Meta:
         model = Driver
         fields = "__all__"
+        widgets = {
+            "date_of_birth": forms.DateInput(attrs={"type": "date"}),
+            "license_exp_date": forms.DateInput(attrs={"type": "date"}),
+            "driver_start_date": forms.DateInput(attrs={"type": "date"}),
+            "driver_end_date": forms.DateInput(attrs={"type": "date"}),
+        }
 
 
 class TruckForm(ModelForm):
     class Meta:
         model = Truck
         fields = "__all__"
+        widgets = {
+            "truck_start_date": forms.DateInput(attrs={"type": "date"}),
+            "truck_end_date": forms.DateInput(attrs={"type": "date"}),
+        }
 
 
 class TrailerForm(ModelForm):
     class Meta:
         model = Trailer
         fields = "__all__"
+        widgets = {
+            "trailer_start_date": forms.DateInput(attrs={"type": "date"}),
+            "trailer_end_date": forms.DateInput(attrs={"type": "date"}),
+        }
 
 
 class LoadForm(ModelForm):
     class Meta:
         model = Load
         fields = "__all__"
+        widgets = {
+            "pickup_date": forms.DateInput(attrs={"type": "date"}),
+            "delivery_date": forms.DateInput(attrs={"type": "date"}),
+        }
 
     def __init__(self, *args, **kwargs):
         driver_id = kwargs.pop("driver_id", None)
